bb90aef958e3d1a29af432137c19831f9ea7255f
Brandon <93067044+DJ45X@users.noreply.github.com>
2023-02-28 13:27:30 -0600

    add central time zone to logging

--- Events/messageCreate/waffles.js
+++ Events/messageCreate/waffles.js
@@ -1,7 +1,8 @@
 module.exports = async (instance, interaction, message) => {
     const now = new Date();
-    const date = now.toLocaleDateString();
-    const time = now.toLocaleTimeString();
+    const options = { timezone: 'America/Chicago' };
+    const date = now.toLocaleDateString('en-us', options);
+    const time = now.toLocaleTimeString('en-us', options);
     if (interaction.content.includes("pancake")) {
         if (interaction.author.bot) return;
         interaction.reply("Waffles are superior! Shut thy face hole pancake lover! 🧇 for life!");
