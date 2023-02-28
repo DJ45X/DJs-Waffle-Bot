1ca171e9162a923b6dddda2cf41afe4a44514464
Brandon <93067044+DJ45X@users.noreply.github.com>
2023-02-28 13:22:46 -0600

    add date, time logging

--- Events/messageCreate/waffles.js
+++ Events/messageCreate/waffles.js
@@ -1,6 +1,10 @@
 module.exports = async (instance, interaction, message) => {
+    const now = new Date();
+    const date = now.toLocaleDateString();
+    const time = now.toLocaleTimeString();
     if (interaction.content.includes("pancake")) {
         if (interaction.author.bot) return;
         interaction.reply("Waffles are superior! Shut thy face hole pancake lover! 🧇 for life!");
+        console.log(`[INFO]: Responded to "pancake" | ${date} - ${time}`);
     }
 };
