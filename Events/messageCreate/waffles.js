68404eaf2aafc252f67f5da01fb5728201d71a63
Brandon <93067044+DJ45X@users.noreply.github.com>
2023-03-01 12:48:57 -0600

    Update waffles.js

--- Events/messageCreate/waffles.js
+++ Events/messageCreate/waffles.js
@@ -3,8 +3,7 @@ module.exports = async (instance, interaction, message) => {
     const options = { timeZone: 'America/Chicago' };
     const date = now.toLocaleDateString('en-us', options);
     const time = now.toLocaleTimeString('en-us', options);
-    const pancake = ['pancake', 'Pancake', 'pancakes', 'Pancakes']
-    if (interaction.content.includes(pancake)) {
+    if (interaction.content.includes('pancake', 'Pancake', 'pancakes', 'Pancakes')) {
         if (interaction.author.bot) return;
         interaction.reply("Waffles are superior! Shut thy face hole pancake lover! 🧇 for life!");
         console.log(`[INFO]: Responded to "pancake" | ${date} - ${time}`);
