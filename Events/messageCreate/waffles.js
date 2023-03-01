22a11f49e94af45f7060c210759a4af6bd13b576
Brandon <93067044+DJ45X@users.noreply.github.com>
2023-03-01 13:34:21 -0600

    Update waffles.js

--- Events/messageCreate/waffles.js
+++ Events/messageCreate/waffles.js
@@ -19,5 +19,13 @@ module.exports = async (instance, interaction, message) => {
         if (interaction.author.bot) return;
         interaction.reply("Waffles are superior! Shut thy face hole pancake lover! 🧇 for life!");
         console.log(`[INFO]: Responded to "pancake" | ${date} - ${time}`);
+    } else if (interaction.content.includes('flapjacks')) {
+        if (interaction.author.bot) return;
+        interaction.reply("Waffles are superior! Shut thy face hole pancake lover! 🧇 for life!");
+        console.log(`[INFO]: Responded to "pancake" | ${date} - ${time}`);
+    } else if (interaction.content.includes('Flapjacks')) {
+        if (interaction.author.bot) return;
+        interaction.reply("Waffles are superior! Shut thy face hole pancake lover! 🧇 for life!");
+        console.log(`[INFO]: Responded to "pancake" | ${date} - ${time}`);
     }
 };
