761936ea886a957daba23a0ce6628381d54fb0fd
Brandon <93067044+DJ45X@users.noreply.github.com>
2023-03-01 12:57:51 -0600

    Update waffles.js

--- Events/messageCreate/waffles.js
+++ Events/messageCreate/waffles.js
@@ -3,7 +3,19 @@ module.exports = async (instance, interaction, message) => {
     const options = { timeZone: 'America/Chicago' };
     const date = now.toLocaleDateString('en-us', options);
     const time = now.toLocaleTimeString('en-us', options);
-    if (interaction.content.includes('pancake', 'Pancake', 'pancakes', 'Pancakes')) {
+    if (interaction.content.includes('pancake')) {
+        if (interaction.author.bot) return;
+        interaction.reply("Waffles are superior! Shut thy face hole pancake lover! 🧇 for life!");
+        console.log(`[INFO]: Responded to "pancake" | ${date} - ${time}`);
+    } else if (interaction.content.includes('Pancake')) {
+        if (interaction.author.bot) return;
+        interaction.reply("Waffles are superior! Shut thy face hole pancake lover! 🧇 for life!");
+        console.log(`[INFO]: Responded to "pancake" | ${date} - ${time}`);
+    } else if (interaction.content.includes('pancakes')) {
+        if (interaction.author.bot) return;
+        interaction.reply("Waffles are superior! Shut thy face hole pancake lover! 🧇 for life!");
+        console.log(`[INFO]: Responded to "pancake" | ${date} - ${time}`);
+    } else if (interaction.content.includes('Pancakes')) {
         if (interaction.author.bot) return;
         interaction.reply("Waffles are superior! Shut thy face hole pancake lover! 🧇 for life!");
         console.log(`[INFO]: Responded to "pancake" | ${date} - ${time}`);
